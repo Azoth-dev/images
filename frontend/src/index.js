4650459a5480e753acb81ee7d0caf5ab2a414399
abhishek <abhusethi@gmail.com>
2022-01-27 15:51:35 +0000

    initial change for image react

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <h1>This from Index.js</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
